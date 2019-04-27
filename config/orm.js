e823578c74e70b7d97bff095bd23fb8a1d19dd21
Kira Helberg <kira.helberg@gmail.com>
2019-04-27 14:31:50 -0400

    debugging

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection");
 
 //In the orm.js file, create the methods that will execute the necessary
 //MySQL commands in the controllers. These are the methods you will need
